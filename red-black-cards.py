8f7a1917b250d9394bdb4f1693a28bcb1d86e0cd
Bruce Xu <brucechanglongxu@gmail.com>
2022-07-21 16:32:14 -0700

    Update red-black-cards.py

--- red-black-cards.py
+++ red-black-cards.py
@@ -4,13 +4,41 @@
 
 import math
 
-black = int(input('How many black cards are there?\n'))
-red = int(input('How many red cards are there?\n'))
+try:
+    print("Input value cannot be cast to an int")
+    exit()
+
+try:
+import math
+
+try:
+    print("Input value cannot be cast to an int")
+    exit()
+
+try:
+    red = int(input('How many red cards are there?\n'))
+except:
+    print("Input value cannot be cast to an int")
+    exit()
 
 def expected_value(b, r):
+    "If either r or b is negative, then we cannot return a valid expected value"
+    if (r < 0 or b < 0):
+        print("Number of red and black cards must both be non-negative")
+        exit()
+    "If r is 0, then we know that the optimal strategy would be to never draw a single card"
+    if (r == 0):
+        return 0
+    "If b is 0, then we know that the optimal strategy would be to keep drawing cards"
+    if (b == 0):
+        return r
+    "If both r and b are equal to 0, then expected value is zero"
     if (b == 0 and r == 0):
         return 0
+    "Otherwise our expected reward is defined by a recursive relation (linearity of expectation)"
     else:
         return max(0, b / (r + b) * (-1 + expected_value(b-1, r)) + r / (r + b) * (1 + expected_value(b, r-1)))
 
-print(expected_value(black, red))
+if __name__ == "__main__":
+    print("The expected value that you would obtain is the following:")
+    print(expected_value(black, red))

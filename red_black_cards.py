4c2e670745bd8e1ad8adffbcde6c5cad397df07b
Bruce Changlong Xu <brucechanglongxu@Bruces-MacBook-Pro.local>
2022-07-24 14:34:50 -0700

    Added doc string to our main function

--- red_black_cards.py
+++ red_black_cards.py
@@ -6,19 +6,28 @@ import math
 
 
 def expected_value(b, r) -> int:
-    "If either r or b is negative, then we cannot return a valid expected value"
+    """
+    Calculates the expected reward if our player plays the optimal strategy in this standard deck of cards 
+    with the number of black and red cards taken in as parameters. The problem statement is given at the top
+    of the file.
+
+    :param 2 integer values: The number of red and black cards existing within our deck of cards
+    :return: The expected reward of our player given that they play with the policy of choosing to draw
+    another card with probability proportional to the ratio of remaining red and black cards
+    """
+    # If either r or b is negative, then we cannot return a valid expected value"
     if r < 0 or b < 0:
         print("Number of red and black cards must both be non-negative")
-    "If r is 0, then we know that the optimal strategy would be to never draw a single card"
+    # If r is 0, then we know that the optimal strategy would be to never draw a single card"
     if r == 0:
         return 0
-    "If b is 0, then we know that the optimal strategy would be to keep drawing cards"
+    # If b is 0, then we know that the optimal strategy would be to keep drawing cards"
     if b == 0:
         return r
-    "If both r and b are equal to 0, then expected value is zero"
+    # If both r and b are equal to 0, then expected value is zero"
     if b == 0 and r == 0:
         return 0
-    "Otherwise our expected reward is defined by a recursive relation (linearity of expectation)"
+    # Otherwise our expected reward is defined by a recursive relation (linearity of expectation)"
     return max(
         0,
         b / (r + b) * (-1 + expected_value(b - 1, r))
@@ -34,6 +43,6 @@ if __name__ == "__main__":
     try:
         red = int(input("How many red cards are there?\n"))
     except:
-        print("Inptu value cannot be cast to an int")
+        print("Input value cannot be cast to an int")
     print("The expected value that you would obtain is the following:")
     print(expected_value(black, red))

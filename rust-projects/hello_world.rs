8b09cc8a2221b5e74979611124e73002fe8ffbd7
Bruce Changlong Xu <brucechanglongxu@Bruces-MacBook-Pro.local>
2022-07-29 09:36:50 -0700

    added rust directory

---
+++ rust-projects/hello_world.rs
@@ -0,0 +1,33 @@
+
+
+
+
+
+// Rust is different from dynamic languages like Ruby, Python and Javascript
+// in the sense that compiling and running the program are two separate steps
+// it is an ahead-of-time compiled language
+// as long as we provide an executable to another user or machine 
+// they will be able to run it
+
+// Compilation is rustc [file]
+// running the file will be ./file
+
+fn main() {
+    println!("Hello World!")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

f968f99c4e119be63ebb31e5c5eaf455072eaf6d
Bruce Xu <brucechanglongxu@gmail.com>
2022-08-26 09:56:19 -0700

    Update first_missing_positive_w1.py

--- interview-problems/first_missing_positive_w1.py
+++ interview-problems/first_missing_positive_w1.py
@@ -1,1 +1,3 @@
-# Given an unsorted integer
+# Given an unsorted integer array nums, return the smallest missing positive integer
+# you must implement an algorithm than runs in O(n) time and uses constant extra space
+# https://leetcode.com/problems/first-missing-positive/

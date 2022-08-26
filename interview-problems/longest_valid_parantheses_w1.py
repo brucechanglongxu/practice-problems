92d488c344dabf9b04378c80b6177cdef23c43ac
Bruce Xu <brucechanglongxu@gmail.com>
2022-08-26 09:54:39 -0700

    Update longest_valid_parantheses_w1.py

--- interview-problems/longest_valid_parantheses_w1.py
+++ interview-problems/longest_valid_parantheses_w1.py
@@ -1,1 +1,2 @@
-
+# Given a string containing just characters '(' and ')' find the length of the longest well-formed paranthesis substring 
+# https://leetcode.com/problems/longest-valid-parentheses/

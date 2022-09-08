48af710d2bfc669e9d28b252593f94a80f47b967
shivatmica <62459136+shivatmica@users.noreply.github.com>
2022-09-08 08:19:04 -0700

    Create longest_valid_parentheses_pytests_shivatmica.py

---
+++ interview-problems/tests/longest_valid_parentheses_pytests_shivatmica.py
@@ -0,0 +1,160 @@
+from interview_problems import longest_valid_parentheses_shivatmica
+
+def test_case_1(sol = longest_valid_parentheses_shivatmica.Solution()) -> None:
+    """
+    Test case 1: Leetcode example test case #1
+    Expected output: 2
+    
+    Parameters
+    ----------
+    sol : object
+        consists of the first_missing_positive question's solution
+        
+    Returns
+    -------
+    None
+    """
+    s = "(()"
+    assert sol.longestValidParentheses(s) == 2
+
+def test_case_2(sol = longest_valid_parentheses_shivatmica.Solution()) -> None:
+    """
+    Test case 2: Leetcode example test case #2
+    Expected output: 4
+    
+    Parameters
+    ----------
+    sol : object
+        consists of the first_missing_positive question's solution
+        
+    Returns
+    -------
+    None
+    """
+    s = ")()())"
+    assert sol.longestValidParentheses(s) == 4
+
+def test_case_3(sol = longest_valid_parentheses_shivatmica.Solution()) -> None:
+    """
+    Test case 3: Leetcode example test case #3
+    Expected output: 0
+    
+    Parameters
+    ----------
+    sol : object
+        consists of the first_missing_positive question's solution
+        
+    Returns
+    -------
+    None
+    """
+    s = ""
+    assert sol.longestValidParentheses(s) == 0
+
+def test_case_4(sol = longest_valid_parentheses_shivatmica.Solution()) -> None:
+    """
+    Test case 4: Leetcode example test case #4
+    Expected output: 4
+    
+    Parameters
+    ----------
+    sol : object
+        consists of the first_missing_positive question's solution
+        
+    Returns
+    -------
+    None
+    """
+    s = "((())"
+    assert sol.longestValidParentheses(s) == 4
+
+def test_case_5(sol = longest_valid_parentheses_shivatmica.Solution()) -> None:
+    """
+    Test case 5: Leetcode example test case #5
+    Expected output: 4
+    
+    Parameters
+    ----------
+    sol : object
+        consists of the first_missing_positive question's solution
+        
+    Returns
+    -------
+    None
+    """
+    s = "())((())"
+    assert sol.longestValidParentheses(s) == 4
+    
+def test_case_6(sol = longest_valid_parentheses_shivatmica.Solution()) -> None:
+    """
+    Test case 6: a string with just left parentheses
+    Expected output: 0
+    
+    Parameters
+    ----------
+    sol : object
+        consists of the first_missing_positive question's solution
+        
+    Returns
+    -------
+    None
+    """
+    s = "((((("
+    assert sol.longestValidParentheses(s) == 0    
+
+def test_case_7(sol = longest_valid_parentheses_shivatmica.Solution()) -> None:
+    """
+    Test case 7: a string with just right parentheses
+    Expected output: 0
+    
+    Parameters
+    ----------
+    sol : object
+        consists of the first_missing_positive question's solution
+        
+    Returns
+    -------
+    None
+    """
+    s = ")))))"
+    assert sol.longestValidParentheses(s) == 0
+    
+def test_case_8(sol = longest_valid_parentheses_shivatmica.Solution()) -> None:
+    """
+    Test case 8: a string containing the maximum number of parentheses as allowed
+    Expected output: 30000
+    
+    Parameters
+    ----------
+    sol : object
+        consists of the first_missing_positive question's solution
+        
+    Returns
+    -------
+    None
+    """
+    s = "(())" * 7500
+    assert sol.longestValidParentheses(s) == 3 * (10**4)
+    
+    
+def main() -> None:
+    """
+    Executes each of the test cases
+    
+    Returns
+    -------
+    None
+    """
+    sol = longest_valid_parentheses_shivatmica.Solution()
+    test_case_1(sol)
+    test_case_2(sol)
+    test_case_3(sol)
+    test_case_4(sol)
+    test_case_5(sol)
+    test_case_6(sol)
+    test_case_7(sol)
+    test_case_8(sol)
+    
+if __name__ == "__main__":
+    main()
+    print("Accepted solution")

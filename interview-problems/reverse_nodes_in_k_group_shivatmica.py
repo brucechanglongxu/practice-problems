63f92384155d9cb7e246833f9a403add7b73f812
shivatmica <62459136+shivatmica@users.noreply.github.com>
2022-09-08 10:05:45 -0700

    Update reverse_nodes_in_k_group_shivatmica.py

--- interview-problems/reverse_nodes_in_k_group_shivatmica.py
+++ interview-problems/reverse_nodes_in_k_group_shivatmica.py
@@ -1,8 +1,9 @@
-# Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+        
 class Solution(object):
     """
     Class for Reverse Nodes in k-Group (#25)

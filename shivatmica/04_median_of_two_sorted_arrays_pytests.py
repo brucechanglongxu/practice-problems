3478d1225fdcac7ce4ffa4705a7316493cda4037
shivatmica <62459136+shivatmica@users.noreply.github.com>
2022-09-11 00:30:53 -0700

    Update 04_median_of_two_sorted_arrays_pytests.py

--- shivatmica/04_median_of_two_sorted_arrays_pytests.py
+++ shivatmica/04_median_of_two_sorted_arrays_pytests.py
@@ -1,6 +1,6 @@
-from interview_problems import median_two_sorted_arrays_shivatmica
+from shivatmica import 04_median_of_two_sorted_arrays
 
-def test_case_1(sol = median_two_sorted_arrays_shivatmica.Solution()) -> None:
+def test_case_1(sol = 04_median_of_two_sorted_arrays.Problem4()) -> None:
     """
     Test case 1: Leetcode example test case #1
     Expected output: 2.00000
@@ -18,7 +18,7 @@ def test_case_1(sol = median_two_sorted_arrays_shivatmica.Solution()) -> None:
     nums2 = [2]
     assert sol.findMedianSortedArrays(nums1, nums2) == 2.00000
 
-def test_case_2(sol = median_two_sorted_arrays_shivatmica.Solution()) -> None:
+def test_case_2(sol = 04_median_of_two_sorted_arrays.Problem4()) -> None:
     """
     Test case 2: Leetcode example test case #2
     Expected output: 2.50000
@@ -36,7 +36,7 @@ def test_case_2(sol = median_two_sorted_arrays_shivatmica.Solution()) -> None:
     nums2 = [3, 4]
     assert sol.findMedianSortedArrays(nums1, nums2) == 2.50000
     
-def test_case_3(sol = median_two_sorted_arrays_shivatmica.Solution()) -> None:
+def test_case_3(sol = 04_median_of_two_sorted_arrays.Problem4()) -> None:
     """
     Test case 3: nums1 array contains the minimum number in the range of possible values for nums1 and nums2 contains the maximum possible number
     Expected output: 0
@@ -54,7 +54,7 @@ def test_case_3(sol = median_two_sorted_arrays_shivatmica.Solution()) -> None:
     nums2 = [10**6]
     assert sol.findMedianSortedArrays(nums1, nums2) == 0
     
-def test_case_4(sol = median_two_sorted_arrays_shivatmica.Solution()) -> None:
+def test_case_4(sol = 04_median_of_two_sorted_arrays.Problem4()) -> None:
     """
     Test case 4: the sorted arrays nums1 and nums2 both contain the maximum amount of elements possible in the arrays
     Expected output: 1000.5
@@ -72,7 +72,7 @@ def test_case_4(sol = median_two_sorted_arrays_shivatmica.Solution()) -> None:
     nums2 = list(range(1001, 2001))
     assert sol.findMedianSortedArrays(nums1, nums2) == 1000.5
     
-def test_case_5(sol = median_two_sorted_arrays_shivatmica.Solution()) -> None:
+def test_case_5(sol = 04_median_of_two_sorted_arrays.Problem4()) -> None:
     """
     Test case 5: nums1 array doesn't contain any elements but nums2 is a sorted array with 6 elements
     Expected output: 7.00000
@@ -90,7 +90,7 @@ def test_case_5(sol = median_two_sorted_arrays_shivatmica.Solution()) -> None:
     nums2 = [2, 4, 6, 8, 10, 12]
     assert sol.findMedianSortedArrays(nums1, nums2) == 7.00000
     
-def test_case_6(sol = median_two_sorted_arrays_shivatmica.Solution()) -> None:
+def test_case_6(sol = 04_median_of_two_sorted_arrays.Problem4()) -> None:
     """
     Test case 6: nums1 is a sorted array with 6 elements and the nums2 array doesn't contain any elements
     Expected output: 7.00000
@@ -108,7 +108,7 @@ def test_case_6(sol = median_two_sorted_arrays_shivatmica.Solution()) -> None:
     nums2 = []
     assert sol.findMedianSortedArrays(nums1, nums2) == 7.00000
     
-def test_case_7(sol = median_two_sorted_arrays_shivatmica.Solution()) -> None:
+def test_case_7(sol = 04_median_of_two_sorted_arrays.Problem4()) -> None:
     """
     Test case 7: nums1 and nums2 are sorted arrays with 1000 elements each, containing the odd and even numbers respectively
     Expected output: 1000.5
@@ -134,7 +134,7 @@ def main() -> None:
     -------
     None
     """
-    sol = median_two_sorted_arrays_shivatmica.Solution()
+    sol = 04_median_of_two_sorted_arrays.Solution()
     test_case_1(sol)
     test_case_2(sol)
     test_case_3(sol)

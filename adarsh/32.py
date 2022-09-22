886b60df9253bd0ecf576b3f1c6b30be3d6ee79a
Adarsh Kumarappan <56987323+Adarsh321123@users.noreply.github.com>
2022-09-22 14:27:35 -0700

    Update 32.py from Mo's review

--- adarsh/32.py
+++ adarsh/32.py
@@ -2,6 +2,13 @@
 def longest_valid_parentheses(s: str = "(()") -> int:
     """
     Finds and returns the length of the longest well-formed parentheses substring in s
+    Algorithm: A stack is used to store the indices of the parentheses. When an open parenthesis is seen, its index is
+    pushed to the stack. When a close parenthesis is seen, the top most element is popped from the stack. If the stack
+    is not empty, the difference between the current index and the index stored at the top of the stack is tracked. If
+    the stack is empty, the current index is pushed to the stack. This process continues until all characters in the
+    input string are processed. The longest difference is the answer.
+    Time complexity: O(n), where n is the length of the input string.
+    Space complexity: O(n), as the stack can have up to the length of the input string.
 
     :param s: String containing just '(' and ')'
     :return: length of the longest well-formed parentheses substring in s
@@ -16,7 +23,8 @@ def longest_valid_parentheses(s: str = "(()") -> int:
         elif stack:
             stack.pop()
             if stack:
-                # idx - stack[-1] finds the length of the substring between the current idx and the one at top of the stack
+                # idx - stack[-1] finds the length of the substring between the current idx and the one at top of the
+                # stack
                 longest = max(longest, idx - stack[-1])
             else:
                 # appending the current idx makes idx - stack[-1] accurate in certain cases

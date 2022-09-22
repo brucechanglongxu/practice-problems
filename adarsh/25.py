25828dc92bd4d9ac28a31430a079955bf1c3605f
Adarsh Kumarappan <56987323+Adarsh321123@users.noreply.github.com>
2022-09-22 14:26:44 -0700

    Update 25.py from Mo's review

--- adarsh/25.py
+++ adarsh/25.py
@@ -1,6 +1,7 @@
 # https://leetcode.com/problems/reverse-nodes-in-k-group/
-# Insights from comments in https://leetcode.com/problems/reverse-nodes-in-k-group/discuss/11653/Python-recursive-and-iterative-solutions-with-comments
-# and https://leetcode.com/problems/reverse-nodes-in-k-group/discuss/11491/Succinct-iterative-Python-O(n)-time-O(1)-space
+# Insights from comments in
+# https://leetcode.com/problems/reverse-nodes-in-k-group/discuss/11653/Python-recursive-and-iterative-solutions-with-comments
+# https://leetcode.com/problems/reverse-nodes-in-k-group/discuss/11491/Succinct-iterative-Python-O(n)-time-O(1)-space
 from typing import Optional
 
 
@@ -9,14 +10,30 @@ class ListNode:
     Definition for singly-linked list
     """
 
-    def __init__(self, val: int = 0, next=None):
+    def __init__(
+            self,
+            val: int = 0,
+            next=None,
+    ):
         self.val = val
         self.next = next
 
 
-def reverse_k_group(head: Optional[ListNode], k: int = 2) -> Optional[ListNode]:
+def reverse_k_group(
+        head: Optional[ListNode],
+        k: int = 2,
+) -> Optional[ListNode]:
     """
     Reverses the nodes of head k at a time and returns that linked list
+    Algorithm: Given the head node of a linked list, check if there are at least k nodes in the linked list. If there
+    isn’t, return the head node. If there is, then reverse k nodes (using standard linked list reversal), and call the
+    function recursively by passing in the node after the kth node as the input parameter. When the recursive call
+    returns, connect the current head node (which is the last node in the current k-group) to the return value. This
+    will connect the tail of the current k-group to the head of the next k-group. At the end of each recursion, return
+    the head of the reversed linked list.
+    Time complexity: O(n), where n is the number of nodes in the input list.
+    Space complexity: O(n/k), as recursion will be called n/k times. As mentioned, k is the number of nodes in a group
+    that need to be reversed.
 
     :param head: Linked list
     :param k: Number of nodes in head to reverse at a time

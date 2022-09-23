6ea1187f237b5aaa469290f8b94c305ba5bfedde
Bruce Xu <brucechanglongxu@gmail.com>
2022-09-22 22:24:48 -0700

    Update valid_parentheses.py

--- bruce-practice-folder/valid_parentheses.py
+++ bruce-practice-folder/valid_parentheses.py
@@ -1,3 +1,11 @@
 class Solution:
     def longestValidParentheses(self, s: str) -> int:
+        stack = []
+        for i, p in enumerate(s):
+            if p == "(":
+                stack.append(i)
+            else:
+                stack.pop()
+                if not stack:
+                    stack.append(i)
         

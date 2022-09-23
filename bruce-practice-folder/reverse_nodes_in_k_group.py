ec8f50a9a423469c95e106ba5c10d77d7703eee8
Bruce Xu <brucechanglongxu@gmail.com>
2022-09-22 20:41:13 -0700

    Update reverse_nodes_in_k_group.py

--- bruce-practice-folder/reverse_nodes_in_k_group.py
+++ bruce-practice-folder/reverse_nodes_in_k_group.py
@@ -1,26 +1,31 @@
 class Node:
-  
     # Constructor to initialize the node object
     def __init__(self, data):
         self.data = data
         self.next = None
   
-  
-class LinkedList:
-  
-    # Function to initialize head
-    def __init__(self):
+class ListNode:
+    """
+    Our definition for our linked list data structure
+    with one node set to a zero value, not pointing to another node.
+    """
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
         self.head = None
-  
-    def reverse(self, head, k):
         
+class Solution:
+    """
+    Main function for reversing k-nodes within our linked list defined, 
+    implementation of a recursive solution. 
+    """
+    def reverseKGroup(self, head: ListNode, k: int) -> ListNode:
         if head == None:
           return None
         current = head
         next = None
         prev = None
         count = 0
-  
         # Reverse first k nodes of the linked list
         while(current is not None and count < k):
             next = current.next
@@ -34,8 +39,7 @@ class LinkedList:
         # from current. And make rest of the list as
         # next of first node
         if next is not None:
-            head.next = self.reverse(next, k)
-  
+            head.next = self.reverseKGroup(next, k)
         # prev is new head of the input list
         return prev
   
@@ -44,10 +48,4 @@ class LinkedList:
         new_node = Node(new_data)
         new_node.next = self.head
         self.head = new_node
-  
-    # Utility function to print the linked LinkedList
-    def printList(self):
-        temp = self.head
-        while(temp):
-            print(temp.data,end=' ')
-            temp = temp.next
+

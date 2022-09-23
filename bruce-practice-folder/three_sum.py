6c02b8e45c31189c9e6a16b1e75c38fdac9c2f90
Bruce Xu <brucechanglongxu@gmail.com>
2022-09-22 18:38:17 -0700

    Update three_sum.py

--- bruce-practice-folder/three_sum.py
+++ bruce-practice-folder/three_sum.py
@@ -1,1 +1,15 @@
-
+def find3Numbers(A, arr_size, sum):
+    # Fix the first element as A[i]
+    for i in range( 0, arr_size-2):
+        # Fix the second element as A[j]
+        for j in range(i + 1, arr_size-1):
+            # Now look for the third number
+            for k in range(j + 1, arr_size):
+                if A[i] + A[j] + A[k] == sum:
+                    print("Triplet is", A[i],
+                          ", ", A[j], ", ", A[k])
+                    return True
+     
+    # If we reach here, then no
+    # triplet was found
+    return False

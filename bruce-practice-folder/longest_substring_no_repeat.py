e28c8e58f5fbe0d974986a46442814d3814a6d47
Bruce Xu <brucechanglongxu@gmail.com>
2022-09-22 19:04:35 -0700

    Update longest_substring_no_repeat.py

--- bruce-practice-folder/longest_substring_no_repeat.py
+++ bruce-practice-folder/longest_substring_no_repeat.py
@@ -1,1 +1,30 @@
-
+def areDistinct(str, i, j):
+ 
+    # Note : Default values in visited are false
+    visited = [0] * (26)
+ 
+    for k in range(i, j + 1):
+        if (visited[ord(str[k]) -
+                   ord('a')] == True):
+            return False
+             
+        visited[ord(str[k]) -
+                ord('a')] = True
+ 
+    return True
+ 
+# Returns length of the longest substring
+# with all distinct characters.
+def longestUniqueSubsttr(str):
+     
+    n = len(str)
+     
+    # Result
+    res = 0
+     
+    for i in range(n):
+        for j in range(i, n):
+            if (areDistinct(str, i, j)):
+                res = max(res, j - i + 1)
+                 
+    return res

036e6a343f5f4a0bfa3066dcdbc9928dfabd1846
Bruce Xu <brucechanglongxu@gmail.com>
2022-09-23 07:04:24 -0700

    Delete hamming_weight.py

--- bruce-practice-folder/hamming_weight.py
+++
@@ -1,8 +0,0 @@
-# Write a function that takes an unsigned integer and returns its hamming weight.
-
-def  countSetBits(n):
-    count = 0
-    while (n):
-        count += n & 1
-        n >>= 1
-    return count

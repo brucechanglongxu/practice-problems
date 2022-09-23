a13fc5ec82f0aa65fc5b85d456cfc584e0a9dc3c
Bruce Xu <brucechanglongxu@gmail.com>
2022-09-22 19:10:16 -0700

    Update reverse_nodes_in_k_group.py

--- bruce-practice-folder/reverse_nodes_in_k_group.py
+++ bruce-practice-folder/reverse_nodes_in_k_group.py
@@ -1,1 +1,53 @@
-
+class Node:
+  
+    # Constructor to initialize the node object
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+  
+  
+class LinkedList:
+  
+    # Function to initialize head
+    def __init__(self):
+        self.head = None
+  
+    def reverse(self, head, k):
+        
+        if head == None:
+          return None
+        current = head
+        next = None
+        prev = None
+        count = 0
+  
+        # Reverse first k nodes of the linked list
+        while(current is not None and count < k):
+            next = current.next
+            current.next = prev
+            prev = current
+            current = next
+            count += 1
+  
+        # next is now a pointer to (k+1)th node
+        # recursively call for the list starting
+        # from current. And make rest of the list as
+        # next of first node
+        if next is not None:
+            head.next = self.reverse(next, k)
+  
+        # prev is new head of the input list
+        return prev
+  
+    # Function to insert a new node at the beginning
+    def push(self, new_data):
+        new_node = Node(new_data)
+        new_node.next = self.head
+        self.head = new_node
+  
+    # Utility function to print the linked LinkedList
+    def printList(self):
+        temp = self.head
+        while(temp):
+            print(temp.data,end=' ')
+            temp = temp.next

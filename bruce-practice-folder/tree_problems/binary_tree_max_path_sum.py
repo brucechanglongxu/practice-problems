357d088dc61970571887f40ce965c79e044499fd
Bruce Xu <brucechanglongxu@gmail.com>
2022-09-23 06:32:54 -0700

    Update and rename bruce-practice-folder/binary_tree_max_path_sum.py to bruce-practice-folder/tree_problems/binary_tree_max_path_sum.py

--- bruce-practice-folder/tree_problems/binary_tree_max_path_sum.py
+++ bruce-practice-folder/tree_problems/binary_tree_max_path_sum.py
@@ -5,7 +5,6 @@
 # Given the root of a binary tree, return the maximum path sum of any non-empty path
 
 class Node:
-     
     # Constructor to create a new node
     def __init__(self, data):
         self.data = data
@@ -14,37 +13,29 @@ class Node:
  
 # This function returns overall maximum path sum in 'res'
 # And returns max path sum going through root
-def findMaxUtil(root):
-     
+def findMaxUtil(root) -> int:
     # Base Case
     if root is None:
         return 0
- 
     # l and r store maximum path sum going through left
     # and right child of root respectively
     l = findMaxUtil(root.left)
     r = findMaxUtil(root.right)
-     
     # Max path for parent call of root. This path
     # must include at most one child of root
     max_single = max(max(l, r) + root.data, root.data)
-     
     # Max top represents the sum when the node under
     # consideration is the root of the maxSum path and
     # no ancestor of root are there in max sum path
     max_top = max(max_single, l+r+ root.data)
- 
     # Static variable to store the changes
     # Store the maximum result
     findMaxUtil.res = max(findMaxUtil.res, max_top)
- 
     return max_single
  
 # Return maximum path sum in tree with given root
-def findMaxSum(root):
-     
+def findMaxSum(root) -> int:
     # Initialize result
     findMaxUtil.res = float("-inf")
-     
     # Compute and return result
     findMaxUtil(root)

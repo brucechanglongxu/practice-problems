f538e03be7237f5528782b425806483f66469213
Bruce Xu <brucechanglongxu@gmail.com>
2022-09-23 13:03:37 -0700

    Delete reverse_nodes_in_kgroup.py

--- interview-problems/reverse_nodes_in_kgroup.py
+++
@@ -1,5 +0,0 @@
-# Given the head of a linked list, reverse the nodes of the list k at a time, and return the modified list. 
-# k is a positive integer and is less than or equal to the length of the linked list.
-# if the number of nodes is not a multiple of k, then left-out nodes, in the end, should remain as is. 
-# https://leetcode.com/problems/reverse-nodes-in-k-group/
-

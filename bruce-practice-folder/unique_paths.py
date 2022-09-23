d4951678e5babc3896bb51151f8f8dad35dd32c5
Bruce Xu <brucechanglongxu@gmail.com>
2022-09-22 18:36:28 -0700

    Update unique_paths.py

--- bruce-practice-folder/unique_paths.py
+++ bruce-practice-folder/unique_paths.py
@@ -1,1 +1,18 @@
+def UniquePathHelper(i, j, r, c, A, paths):
+    # boundary condition or constraints
+    if (i == r or j == c):
+        return 0
+ 
+    if (A[i][j] == 1):
+        return 0
+ 
+    # base case
+    if (i == r - 1 and j == c - 1):
+        return 1
+ 
+    if (paths[i][j] != -1):
+        return paths[i][j]
 
+    paths[i][j] = UniquePathHelper(
+        i + 1, j, r, c, A, paths) + UniquePathHelper(i, j + 1, r, c, A, paths)
+    return paths[i][j]

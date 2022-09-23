e6113a318e9aa3ec4eaa45895237f96bb9022b6c
Bruce Xu <brucechanglongxu@gmail.com>
2022-09-22 18:51:51 -0700

    Update rotate_image.py

--- bruce-practice-folder/rotate_image.py
+++ bruce-practice-folder/rotate_image.py
@@ -1,1 +1,17 @@
-
+def rotateMatrix(mat):
+    # reversing the matrix
+    for i in range(len(mat)):
+        mat[i].reverse()
+    # make transpose of the matrix
+    for i in range(len(mat)):
+        for j in range(i, len(mat)):
+            # swapping mat[i][j] and mat[j][i]
+            mat[i][j], mat[j][i] = mat[j][i], mat[i][j]
+ 
+ 
+# Function to print the matrix
+def displayMatrix(mat):
+    for i in range(0, len(mat)):
+        for j in range(0, len(mat)):
+            print(mat[i][j], end=' ')
+        print()

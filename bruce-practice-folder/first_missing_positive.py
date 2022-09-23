48813f74472108843b68c78db673aafaf9c089f8
Bruce Xu <brucechanglongxu@gmail.com>
2022-09-22 21:50:48 -0700

    Update first_missing_positive.py

--- bruce-practice-folder/first_missing_positive.py
+++ bruce-practice-folder/first_missing_positive.py
@@ -10,7 +10,6 @@ def segregate(arr, size):
 ''' Find the smallest positive missing number
 in an array that contains all positive integers '''
 def findMissingPositive(arr, size):
-     
     # Mark arr[i] as visited by
     # making arr[arr[i] - 1] negative.
     # Note that 1 is subtracted
@@ -32,10 +31,22 @@ def findMissingPositive(arr, size):
 number in an array that contains
 both positive and negative integers '''
 def findMissing(arr, size):
-     
     # First separate positive and negative numbers
     shift = segregate(arr, size)
-     
     # Shift the array and call findMissingPositive for
     # positive part
     return findMissingPositive(arr[shift:], size - shift)
+
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        # Append 0 so that we do not return this as the minimum positive integer
+        nums.append(0)
+        # Obtain the length of our array
+        N = len(nums)
+        arr = [0 for _ in range(N)]
+        # Iterate through all the numbers in our array, and add onto our returned array
+        for i in range(N):
+            if 0 < nums[i] <= N:
+                arr[nums[i] - 1] += 1
+        for i in range(N):
+            if arr[i] == 0: return i + 1

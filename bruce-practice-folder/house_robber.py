e709b97b3abe026156f3aca29069aab768b94094
Bruce Xu <brucechanglongxu@gmail.com>
2022-09-22 18:54:05 -0700

    Update house_robber.py

--- bruce-practice-folder/house_robber.py
+++ bruce-practice-folder/house_robber.py
@@ -1,1 +1,18 @@
-
+def maxLoot(hval,n):
+ 
+    # base case
+    if (n < 0):
+        return 0
+ 
+    if (n == 0):
+        return hval[0]
+     
+    # if current element is pick then previous cannot be
+    # picked
+    pick = hval[n] + maxLoot(hval, n - 2)
+    # if current element is not picked then previous
+    # element is picked
+    notPick = maxLoot(hval, n - 1)
+ 
+    # return max of picked and not picked
+    return max(pick, notPick)

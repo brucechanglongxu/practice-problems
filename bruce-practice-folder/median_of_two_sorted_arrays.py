3705482b7382a70966dc8b5df3e8e5689e150e10
Bruce Xu <brucechanglongxu@gmail.com>
2022-09-22 19:06:29 -0700

    Update median_of_two_sorted_arrays.py

--- bruce-practice-folder/median_of_two_sorted_arrays.py
+++ bruce-practice-folder/median_of_two_sorted_arrays.py
@@ -1,1 +1,17 @@
-
+def Solution(arr):
+ 
+    n = len(arr)
+ 
+    # If length of array is even
+    if n % 2 == 0:
+        z = n // 2
+        e = arr[z]
+        q = arr[z - 1]
+        ans = (e + q) / 2
+        return ans
+         
+    # If length of array is odd
+    else:
+        z = n // 2
+        ans = arr[z]
+        return ans

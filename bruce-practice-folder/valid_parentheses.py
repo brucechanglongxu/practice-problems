0e4e1b949de8e9324bbc7a6a7ff40e469eb854b4
Bruce Xu <brucechanglongxu@gmail.com>
2022-09-22 22:23:24 -0700

    Update valid_parentheses.py

--- bruce-practice-folder/valid_parentheses.py
+++ bruce-practice-folder/valid_parentheses.py
@@ -1,28 +1,3 @@
-def areBracketsBalanced(expr):
-    stack = []
-    # Traversing the Expression
-    for char in expr:
-        if char in ["(", "{", "["]:
-            # Push the element in the stack
-            stack.append(char)
-        else:
-            # If current character is not opening
-            # bracket, then it must be closing.
-            # So stack cannot be empty at this point.
-            if not stack:
-                return False
-            current_char = stack.pop()
-            if current_char == '(':
-                if char != ")":
-                    return False
-            if current_char == '{':
-                if char != "}":
-                    return False
-            if current_char == '[':
-                if char != "]":
-                    return False
- 
-    # Check Empty Stack
-    if stack:
-        return False
-    return True
+class Solution:
+    def longestValidParentheses(self, s: str) -> int:
+        

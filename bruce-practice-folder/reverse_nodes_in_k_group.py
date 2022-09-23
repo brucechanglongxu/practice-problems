f915dad673ef62a457bdd036c6627cca067870a5
Bruce Xu <brucechanglongxu@gmail.com>
2022-09-22 20:49:58 -0700

    Update reverse_nodes_in_k_group.py

--- bruce-practice-folder/reverse_nodes_in_k_group.py
+++ bruce-practice-folder/reverse_nodes_in_k_group.py
@@ -48,4 +48,37 @@ class Solution:
         new_node = Node(new_data)
         new_node.next = self.head
         self.head = new_node
-
+        
+        
+class SolutionNonRecursive: 
+    """
+    A non-recursive solution
+    """
+    def reverseKGroup(self, head: ListNode, k: int) -> ListNode:
+        dummy = ListNode(0, head)
+        groupPrev = dummy
+        
+        while True:
+            self.getKth()
+            if not kth:
+                break
+            groupNext = kth.next 
+            
+            # reverse a group (with two-pointer method)
+            prev, curr = kth.next, groupPrev.next
+            while curr != groupNext:
+                tmp = curr.next
+                curr.next = prev
+                prev = curr
+                curr = tmp
+                
+            tmp = groupPrev.next     
+            groupPrev.next = kth
+            groupPrev = tmp
+        return dummy.next
+            
+        def getKth(self, curr, k):
+            while curr and k > 0:
+                curr = curr.next
+                k -= 1
+            return curr 

0029e24a2c10dd0762d91f03614ea5f3c7bece6b
Bruce Xu <brucechanglongxu@gmail.com>
2022-09-22 18:56:45 -0700

    Update contains_duplicate.py

--- bruce-practice-folder/contains_duplicate.py
+++ bruce-practice-folder/contains_duplicate.py
@@ -1,1 +1,17 @@
-
+def checkDuplicatesWithinK(arr,  n,  k):
+ 
+    # traversing the input array
+    for i in range(n):
+        j = i + 1
+        range_ = k
+         
+        # searching in next k-1 elements if its duplicate
+        # is present or not
+        while (range_ > 0 and j < n):
+            if (arr[i] == arr[j]):
+                return True
+            j += 1
+            range_ -= 1
+ 
+    return False
+ 

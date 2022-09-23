ec3c2718369348107f58183ab00381c565b38cfd
Bruce Xu <brucechanglongxu@gmail.com>
2022-09-22 22:12:09 -0700

    Update median_of_two_sorted_arrays.py

--- bruce-practice-folder/median_of_two_sorted_arrays.py
+++ bruce-practice-folder/median_of_two_sorted_arrays.py
@@ -1,8 +1,13 @@
 class Solution:
  def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
- A, B = nums1, nums2
- total = len(nums1) + len(nums2)
- half = total // 2
- if len(B) < len(A):
-  A, B = B, A
+    A, B = nums1, nums2
+    total = len(nums1) + len(nums2)
+    half = total // 2
+    if len(B) < len(A):
+       A, B = B, A
+    l, r = 0, len(A) - 1
+    while True:
+       i = (l + r) // 2
+       j = half - i - 2
+ 
  

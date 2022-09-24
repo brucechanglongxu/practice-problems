81bbff0ac1b52f19eab391f7b09131f903943f32
Bruce Xu <brucechanglongxu@gmail.com>
2022-09-24 16:46:01 -0700

    Update reverse_nodes_kgroup_test.py

--- bruce/tests/reverse_nodes_kgroup_test.py
+++ bruce/tests/reverse_nodes_kgroup_test.py
@@ -1,29 +1,42 @@
 import reverse_nodes_in_k_group
 import array_to_list
+import list_to_array
 
 def first_test_case(reversed_sol : reverse_nodes_in_k_group.Solution) -> None:
+    """
+    Reversing all of the nodes at once
+    """
+    orig_list = [1, 2, 3, 4, 5, 6, 7, 8]
+    k = 8
+    list_linked = array_to_list(orig_list)
+    assert reversed_sol.reverse_nodes_in_k_group(list_to_array(list_linked)) == [8, 7, 6, 5, 4, 3, 2, 1]
+   
+def second_test_case(reversed_sol : reverse_nodes_in_k_group.Solution) -> None:
+    """
+    Reversing n/4 of all of the nodes
+    """
+    orig_list = [1, 2, 3, 4, 5, 6, 7, 8]
+    k = 2
+    list_linked = array_to_list(orig_list)
+    assert reversed_sol.reverse_nodes_in_k_group(list_to_array(list_linked)) == [2, 1, 4, 3, 6, 5, 8 ,7]
+
+def third_test_case(reversed_sol : reverse_nodes_in_k_group.Solution) -> None:
+    """
+    Reversing half of all of the nodes
+    """
+    orig_list = [1, 2, 3, 4, 5, 6, 7, 8]
+    k = 4
+    list_linked = array_to_list(orig_list)
+    assert reversed_sol.reverse_nodes_in_k_group(list_to_array(list_linked)) == [4, 3, 2, 1, 8, 7, 6, 5]
+    
+def fourth_test_case(reversed_sol : reverse_nodes_in_k_group.Solution) -> None:
+    """
+    Reversing only one node at a time
+    """
     orig_list = [1, 2, 3, 4, 5, 6, 7, 8]
     k = 1
     list_linked = array_to_list(orig_list)
-    assert reversed_sol.reverse_nodes_in_k_group(list_linked) == 0
-   
-def second_test_case(reversed_sol : reverse_nodes_in_k_group.Solution) -> None:
-    orig_list = []
-    assert reversed_sol.reverse_nodes_in_k_group(list_linked) == 0
-
-def third_test_case(reversed_sol : reverse_nodes_in_k_group.Solution) -> None:
-    orig_list = []
-    list_linked
-    assert reversed_sol.reverse_nodes_in_k_group(list_linked) == 0
-    
-def fourth_test_case(reversed_sol : reverse_nodes_in_k_group.Solution) -> None:
-    orig_list = []
-    list_linked = []
-    assert reversed_sol.reverse_nodes_in_k_group(list_linked) == 0
-    
-def fifth_test_case(reversed_sol : reverse_nodes_in_k_group.Solution) -> None:
-    list = []
-    assert reversed_sol.reverse_nodes_in_k_group(list_linked) == 0
+    assert reversed_sol.reverse_nodes_in_k_group(list_to_array(list_linked)) == [1, 2, 3, 4, 5, 6, 7, 8]
     
 def main() -> None:
     """
@@ -35,7 +48,6 @@ def main() -> None:
     second_test_case(solution)
     third_test_case(solution)
     fourth_test_case(solution)
-    fifth_test_case(solution)
 
 if __name__ == "__main__":
     main()

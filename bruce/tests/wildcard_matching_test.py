40593fb5081b70e17aa014542d663fa5d6c103dc
Bruce Xu <brucechanglongxu@gmail.com>
2022-09-24 14:15:14 -0700

    Update wildcard_matching_test.py

--- bruce/tests/wildcard_matching_test.py
+++ bruce/tests/wildcard_matching_test.py
@@ -3,123 +3,76 @@ import wildcard_matching
 
 def test_case_1(sol: wildcard_matching.Solution) -> None:
     """
-    Test case. Create an input string and pattern string with distinct characters that don't have the same length. Note that this is LeetCode example test case 1.
-    Verify that the pattern does not match the string.
-    :param sol: Object with is_match solution method
+    Test case. Comparison with the empty string.
+    :param sol: Object with is_match solution method.
     :return: None
     """
     s = "aa"
-    p = "a"
+    p = ""
     assert sol.is_match(s, p) == False
-
 
 def test_case_2(sol: wildcard_matching.Solution) -> None:
     """
-    Test case. Create an input string with distinct characters and a pattern string with only a star. Note that this is LeetCode example test case 2.
-    Verify that the pattern matches the string.
-    :param sol: Object with is_match solution method
-    :return: None
-    """
-    s = "aa"
-    p = "*"
-    assert sol.is_match(s, p) == True
-
-
-def test_case_3(sol: wildcard_matching.Solution) -> None:
-    """
-    Test case. Create an input string with distinct characters and the pattern string containing a question mark. Note that this is LeetCode example test case 3.
-    Verify that the pattern does not match the string.
-    :param sol: Object with is_match solution method
-    :return: None
-    """
-    s = "cb"
-    p = "?a"
-    assert sol.is_match(s, p) == False
-
-
-def test_case_4(sol: wildcard_matching.Solution) -> None:
-    """
-    Test case. Create an input string with distinct characters and a pattern string with multiple stars.
-    Verify that the pattern matches the string.
-    :param sol: Object with is_match solution method
-    :return: None
-    """
-    s = "abcdefg"
-    p = "**********"
-    assert sol.is_match(s, p) == True
-
-
-def test_case_5(sol: wildcard_matching.Solution) -> None:
-    """
-    Test case. Create an input string and pattern string with the same distinct characters.
-    Verify that the pattern matches the string.
-    :param sol: Object with is_match solution method
-    :return: None
-    """
-    s = "abcdefg"
-    p = "abcdefg"
-    assert sol.is_match(s, p) == True
-
-
-def test_case_6(sol: wildcard_matching.Solution) -> None:
-    """
-    Test case. Create an input string with distinct characters and a pattern string with all question marks.
-    Verify that the pattern matches the string.
-    :param sol: Object with is_match solution method
-    :return: None
-    """
-    s = "abcdefg"
-    p = "???????"
-    assert sol.is_match(s, p) == True
-
-
-def test_case_7(sol: wildcard_matching.Solution) -> None:
-    """
-    Test case. Create an input string with distinct characters and a pattern string with distinct characters, question marks, and a star.
-    Verify that the pattern matches the string.
-    :param sol: Object with is_match solution method
-    :return: None
-    """
-    s = "abcdefg"
-    p = "??c?e?g*"
-    assert sol.is_match(s, p) == True
-
-
-def test_case_8(sol: wildcard_matching.Solution) -> None:
-    """
-    Test case. Create an input string with distinct characters and a pattern string with distinct characters, question marks, and stars.
-    Verify that the pattern does not match the string.
-    :param sol: Object with is_match solution method
-    :return: None
-    """
-    s = "abcdefg"
-    p = "??*h?g*"
-    assert sol.is_match(s, p) == False
-
-
-def test_case_9(sol: wildcard_matching.Solution) -> None:
-    """
-    Test case. Create an input string and pattern string with empty strings. This tests the low end of the range of constraints in the problem.
-    Verify that the pattern matches the string.
-    :param sol: Object with is_match solution method
+    Test case. Comparison of two empty strings.
+    :param sol: Object with is_match solution method.
     :return: None
     """
     s = ""
     p = ""
     assert sol.is_match(s, p) == True
 
-
-def test_case_10(sol: wildcard_matching.Solution) -> None:
+def test_case_3(sol: wildcard_matching.Solution) -> None:
     """
-    Test case. Create an input string with 2000 distinct characters and a pattern string with only one star. This tests the high end of the range of constraints in the problem.
-    Verify that the pattern matches the string.
+    Test case. Comparison of star and empty string.
+    :param sol: Object with is_match solution method.
+    :return: None
+    """
+    s = "*"
+    p = ""
+    assert sol.is_match(s, p) == True
+
+def test_case_4(sol: wildcard_matching.Solution) -> None:
+    """
+    Test case. Comparison of multiple stars sandwhiching a singular character. 
     :param sol: Object with is_match solution method
     :return: None
     """
-    s = "z" * 2000
-    p = "*"
+    s = "aaaa"
+    p = "*a*"
     assert sol.is_match(s, p) == True
 
+
+def test_case_5(sol: wildcard_matching.Solution) -> None:
+    """
+    Test case. Comparison with all question marks
+    :param sol: Object with is_match solution method
+    :return: None
+    """
+    s = "abcde"
+    p = "?????"
+    assert sol.is_match(s, p) == True
+
+
+def test_case_6(sol: wildcard_matching.Solution) -> None:
+    """
+    Test case. Comparison with only stars and question marks
+    :param sol: Object with is_match solution method
+    :return: None
+    """
+    s = "abcde"
+    p = "?????"
+    assert sol.is_match(s, p) == True
+
+
+def test_case_7(sol: wildcard_matching.Solution) -> None:
+    """
+    Test case. Star and question mark sandwhiched between a number and character. 
+    :param sol: Object with is_match solution method
+    :return: None
+    """
+    s = "abcd12"
+    p = "*1?"
+    assert sol.is_match(s, p) == True
 
 def main() -> None:
     """
@@ -134,11 +87,8 @@ def main() -> None:
     test_case_5(sol)
     test_case_6(sol)
     test_case_7(sol)
-    test_case_8(sol)
-    test_case_9(sol)
-    test_case_10(sol)
 
 
 if __name__ == "__main__":
     main()
-    print("Our solution passes")
+    print("Our solution is robust and passes")

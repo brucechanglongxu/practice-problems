a02de80d4b2b1da8bd1c5b946a1242dcb35b4564
Bruce Xu <brucechanglongxu@gmail.com>
2022-09-24 16:17:14 -0700

    Update first_missing_positive_test.py

--- bruce/tests/first_missing_positive_test.py
+++ bruce/tests/first_missing_positive_test.py
@@ -8,26 +8,33 @@ def first_test_case(sol: first_missing_positive.Solution) -> None:
     assert sol.first_missing_positive(arr) == 7
     
 
-def first_test_case(sol: first_missing_positive.Solution) -> None:
+def second_test_case(sol: first_missing_positive.Solution) -> None:
     """
     Tests for an array of consecutive elements missing the number 1
     """
     arr  = [2, 3, 4, 5, 6, 7]
     assert sol.first_missing_positive(arr) == 1
     
-def first_test_case(sol: first_missing_positive.Solution) -> None:
+def third_test_case(sol: first_missing_positive.Solution) -> None:
     """
     Tests for an array that consists entirely of negative numbers
     """
     arr = [-1, -2, -3, -4]
     assert sol.first_missing_positive(arr) == 1
     
-def first_test_case(sol: first_missing_positive.Solution) -> None:
+def fourth_test_case(sol: first_missing_positive.Solution) -> None:
     """
     Tests for an array that is a mixture of positive and negative numbers
     """
     arr = [-1, -2, 1, 2]
     assert sol.first_missing_positive(arr) == 3
+
+def main() -> None:
+    solution = first_missing_positive.Solution()
+    first_test_case(solution)
+    second_test_case(solution)
+    third_test_case(solution)
+    fourth_test_case(solution)
 
 if __name__ == "__main__":
     main()

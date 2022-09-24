a166a5cb15e67790f6dab8b2d2396db39f9710bb
Bruce Xu <brucechanglongxu@gmail.com>
2022-09-24 16:15:53 -0700

    Update first_missing_positive_test.py

--- bruce/tests/first_missing_positive_test.py
+++ bruce/tests/first_missing_positive_test.py
@@ -1,15 +1,33 @@
 from bruce import first_missing_positive
 
-
-
-
-
-def main() -> None:
+def first_test_case(sol: first_missing_positive.Solution) -> None:
     """
-    :return: None
+    Tests for an array of consecutive elements from 1 through to N
     """
-    sol = first_missing_positive.Solution()
+    arr = [1, 2, 3, 4, 5, 6]
+    assert sol.first_missing_positive(arr) == 7
+    
 
+def first_test_case(sol: first_missing_positive.Solution) -> None:
+    """
+    Tests for an array of consecutive elements missing the number 1
+    """
+    arr  = [2, 3, 4, 5, 6, 7]
+    assert sol.first_missing_positive(arr) == 1
+    
+def first_test_case(sol: first_missing_positive.Solution) -> None:
+    """
+    Tests for an array that consists entirely of negative numbers
+    """
+    arr = [-1, -2, -3, -4]
+    assert sol.first_missing_positive(arr) == 1
+    
+def first_test_case(sol: first_missing_positive.Solution) -> None:
+    """
+    Tests for an array that is a mixture of positive and negative numbers
+    """
+    arr = [-1, -2, 1, 2]
+    assert sol.first_missing_positive(arr) == 3
 
 if __name__ == "__main__":
     main()

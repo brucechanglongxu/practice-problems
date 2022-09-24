9b576a04f69fe0158e52a68889d92434a646fb81
Bruce Xu <brucechanglongxu@gmail.com>
2022-09-24 14:31:44 -0700

    Update median_of_two_sorted_arrays.py

--- bruce/median_of_two_sorted_arrays.py
+++ bruce/median_of_two_sorted_arrays.py
@@ -1,30 +1,32 @@
 class Solution:
- def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-    """
-    Solution that partitions our two array and merges the two appropriately
-    to find the outputted array which will provide us with the median.
-    """
-    A, B = nums1, nums2
-    total = len(nums1) + len(nums2)
-    half = total // 2
-    if len(B) < len(A):
-       A, B = B, A
-    l, r = 0, len(A) - 1
-    while True:
-       i = (l + r) // 2 # A
-       j = half - i - 2 # B
-       Aleft = A[i] if i >= 0 else float("-infinity")
-       Aright = A[i + 1] if i < len(A) else float("infinity")
-       Bleft = B[j] if j >= 0 else float("-infinity")
-       Bright = B[j + 1] if (j + 1) < len(B)
-       # partition is correct
-       if Aleft <= Bright and Bleft <= Aright:
-          # add
-          if total % 2:
-             return min(Aright, Bright)
-          # even
-          return (max(Aleft, Bleft) + min(Aright, Bright)) / 2
-       elif Aleft > Bright:
-          r = i - 1
-       else:
-          l = i + 1
+        def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        """
+        Solution that partitions our two array and merges the two appropriately
+        to find the outputted array which will provide us with the median.
+        Inputs : Two sorted lists of integers
+        Output: A floating point number that denotes the median element between both of the lists
+        """
+        A, B = nums1, nums2
+        total = len(nums1) + len(nums2)
+        half = total // 2
+        if len(B) < len(A):
+                A, B = B, A
+        l, r = 0, len(A) - 1
+        while True:
+                i = (l + r) // 2 # A
+                j = half - i - 2 # B
+                Aleft = A[i] if i >= 0 else float("-infinity")
+                Aright = A[i + 1] if i < len(A) else float("infinity")
+                Bleft = B[j] if j >= 0 else float("-infinity")
+                Bright = B[j + 1] if (j + 1) < len(B)
+                # partition is correct
+                if Aleft <= Bright and Bleft <= Aright:
+                        # add
+                        if total % 2:
+                                return min(Aright, Bright)
+                        # even
+                return (max(Aleft, Bleft) + min(Aright, Bright)) / 2
+                elif Aleft > Bright:
+                        r = i - 1
+                else:
+                        l = i + 1

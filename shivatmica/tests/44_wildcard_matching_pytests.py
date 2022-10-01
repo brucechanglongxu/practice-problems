3e8ddf98f6a5158d432063216d58dd6558467653
shivatmica <62459136+shivatmica@users.noreply.github.com>
2022-09-30 23:02:56 -0700

    update docstring format

--- shivatmica/tests/44_wildcard_matching_pytests.py
+++ shivatmica/tests/44_wildcard_matching_pytests.py
@@ -5,14 +5,8 @@ def test_case_1(sol = 44_wildcard_matching.Problem44()) -> None:
     Test case 1: both the sequence and the pattern are empty strings
     Expected output: True
     
-    Parameters
-    ----------
-    sol : object
-        consists of the first_missing_positive question's solution
-        
-    Returns
-    -------
-    None
+    :param sol: consists of the first_missing_positive question's solution
+    :return: None
     """
     s = ""
     p = ""
@@ -23,14 +17,8 @@ def test_case_2(sol = 44_wildcard_matching.Problem44()) -> None:
     Test case 2: a string of characters compared to the pattern with only stars
     Expected output: True
     
-    Parameters
-    ----------
-    sol : object
-        consists of the first_missing_positive question's solution
-        
-    Returns
-    -------
-    None
+    :param sol: consists of the first_missing_positive question's solution
+    :return: None
     """
     s = "testcase"
     p = "****"
@@ -41,14 +29,8 @@ def test_case_3(sol = 44_wildcard_matching.Problem44()) -> None:
     Test case 3: a string of characters compared to a pattern with an equivalent number of question marks
     Expected output: True
     
-    Parameters
-    ----------
-    sol : object
-        consists of the first_missing_positive question's solution
-        
-    Returns
-    -------
-    None
+    :param sol: consists of the first_missing_positive question's solution
+    :return: None
     """
     s = "testcase"
     p = "????????"
@@ -59,14 +41,8 @@ def test_case_4(sol = 44_wildcard_matching.Problem44()) -> None:
     Test case 4: a string of characters compared to a pattern with the same characters
     Expected output: True
     
-    Parameters
-    ----------
-    sol : object
-        consists of the first_missing_positive question's solution
-        
-    Returns
-    -------
-    None
+    :param sol: consists of the first_missing_positive question's solution
+    :return: None
     """
     s = "testcase"
     p = "testcase"
@@ -77,14 +53,8 @@ def test_case_5(sol = 44_wildcard_matching.Problem44()) -> None:
     Test case 5: a string of characters compared to a pattern with some characters, stars, and question marks
     Expected output: True
     
-    Parameters
-    ----------
-    sol : object
-        consists of the first_missing_positive question's solution
-        
-    Returns
-    -------
-    None
+    :param sol: consists of the first_missing_positive question's solution
+    :return: None
     """
     s = "testcase"
     p = "*e?t?a??"
@@ -95,14 +65,8 @@ def test_case_6(sol = 44_wildcard_matching.Problem44()) -> None:
     Test case 6: a string with the maximum number of possible characters and a pattern of a string of stars
     Expected output: True
     
-    Parameters
-    ----------
-    sol : object
-        consists of the first_missing_positive question's solution
-        
-    Returns
-    -------
-    None
+    :param sol: consists of the first_missing_positive question's solution
+    :return: None
     """
     s = "a" * 2000
     p = "***"
@@ -114,14 +78,8 @@ def test_case_7(sol = 44_wildcard_matching.Problem44()) -> None:
     Test case 7: Leetcode example test case #1
     Expected output: False
     
-    Parameters
-    ----------
-    sol : object
-        consists of the first_missing_positive question's solution
-        
-    Returns
-    -------
-    None
+    :param sol: consists of the first_missing_positive question's solution
+    :return: None
     """
     s = "aa"
     p = "a"
@@ -132,14 +90,8 @@ def test_case_8(sol = 44_wildcard_matching.Problem44()) -> None:
     Test case 8: Leetcode example test case #2
     Expected output: True
     
-    Parameters
-    ----------
-    sol : object
-        consists of the first_missing_positive question's solution
-        
-    Returns
-    -------
-    None
+    :param sol: consists of the first_missing_positive question's solution
+    :return: None
     """
     s = "aa"
     p = "*"
@@ -150,14 +102,8 @@ def test_case_9(sol = 44_wildcard_matching.Problem44()) -> None:
     Test case 9: Leetcode example test case #3
     Expected output: False
     
-    Parameters
-    ----------
-    sol : object
-        consists of the first_missing_positive question's solution
-        
-    Returns
-    -------
-    None
+    :param sol: consists of the first_missing_positive question's solution
+    :return: None
     """
     s = "cb"
     p = "?a"
@@ -168,14 +114,8 @@ def test_case_10(sol = 44_wildcard_matching.Problem44()) -> None:
     Test case 10: Leetcode example test case #4
     Expected output: True
     
-    Parameters
-    ----------
-    sol : object
-        consists of the first_missing_positive question's solution
-        
-    Returns
-    -------
-    None
+    :param sol: consists of the first_missing_positive question's solution
+    :return: None
     """
     s = "adceb"
     p = "*a*b"
@@ -185,9 +125,7 @@ def main() -> None:
     """
     Executes each of the test cases
     
-    Returns
-    -------
-    None
+    :return: None
     """
     sol = 44_wildcard_matching.Problem44()
     test_case_1(sol)

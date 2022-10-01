49a227237e2954d2a7b0ba67c8910a657cecfe45
shivatmica <62459136+shivatmica@users.noreply.github.com>
2022-09-30 22:58:17 -0700

    update docstring format

--- shivatmica/tests/25_reverse_nodes_in_k_group_pytests.py
+++ shivatmica/tests/25_reverse_nodes_in_k_group_pytests.py
@@ -28,14 +28,8 @@ def test_case_1(sol = 25_reverse_nodes_in_k_group.Problem25()) -> None:
     Test case 1: Leetcode example test case #1
     Expected output: [2, 1, 4, 3, 5]
     
-    Parameters
-    ----------
-    sol : object
-        consists of the reverse_nodes_in_k_group question's solution
-        
-    Returns
-    -------
-    None
+    :param sol: consists of the reverse_nodes_in_k_group question's solution
+    :return: None
     """
     list = [1, 2, 3, 4, 5]
     k = 2
@@ -47,14 +41,8 @@ def test_case_2(sol = 25_reverse_nodes_in_k_group.Problem25()) -> None:
     Test case 2: Leetcode example test case #2
     Expected output: [3, 2, 1, 4, 5]
     
-    Parameters
-    ----------
-    sol : object
-        consists of the reverse_nodes_in_k_group question's solution
-        
-    Returns
-    -------
-    None
+    :param sol: consists of the reverse_nodes_in_k_group question's solution
+    :return: None
     """
     list = [1, 2, 3, 4, 5]
     k = 3
@@ -66,14 +54,8 @@ def test_case_3(sol = 25_reverse_nodes_in_k_group.Problem25()) -> None:
     Test case 3: linked list with 5000 nodes, the maximum number of nodes possible in the list
     Expected output: [5, 4, 3, 2, 1, 5, 4, 3, 2, 1] * 500
     
-    Parameters
-    ----------
-    sol : object
-        consists of the reverse_nodes_in_k_group question's solution
-        
-    Returns
-    -------
-    None
+    :param sol: consists of the reverse_nodes_in_k_group question's solution
+    :return: None
     """
     list = [1, 2, 3, 4, 5] * 1000
     k = 10
@@ -85,14 +67,8 @@ def test_case_4(sol = 25_reverse_nodes_in_k_group.Problem25()) -> None:
     Test case 4: linked list with 1 node with the value 0 (minimum values in the ranges for k, n, and node.val)
     Expected output: [0]
     
-    Parameters
-    ----------
-    sol : object
-        consists of the reverse_nodes_in_k_group question's solution
-        
-    Returns
-    -------
-    None
+    :param sol: consists of the reverse_nodes_in_k_group question's solution
+    :return: None
     """
     list = [0]
     k = 1
@@ -104,14 +80,8 @@ def test_case_5(sol = 25_reverse_nodes_in_k_group.Problem25()) -> None:
     Test case 5: linked list with 5000 nodes, the maximum number of nodes possible in the list and also includes the maximum value for node.val (1000)
     Expected output: [1000, 999, 998, 997, 996, 1000, 999, 998, 997, 996] * 500
     
-    Parameters
-    ----------
-    sol : object
-        consists of the reverse_nodes_in_k_group question's solution
-        
-    Returns
-    -------
-    None
+    :param sol: consists of the reverse_nodes_in_k_group question's solution
+    :return: None
     """
     list = [996, 997, 998, 999, 1000] * 1000
     k = 10
@@ -123,14 +93,8 @@ def test_case_5(sol = 25_reverse_nodes_in_k_group.Problem25()) -> None:
     Test case 6: linked list with 5000 nodes, the maximum number of nodes possible in the list and the maximum value for k (5000)
     Expected output: [14] * 5000
     
-    Parameters
-    ----------
-    sol : object
-        consists of the reverse_nodes_in_k_group question's solution
-        
-    Returns
-    -------
-    None
+    :param sol: consists of the reverse_nodes_in_k_group question's solution
+    :return: None
     """
     list = [14] * 5000
     k = 5000
@@ -141,9 +105,7 @@ def main() -> None:
     """
     Executes each of the test cases
     
-    Returns
-    -------
-    None
+    :return: None
     """
     sol = 25_reverse_nodes_in_k_group.Problem25()
     test_case_1(sol)
